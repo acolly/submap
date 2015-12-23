32b21f6af0e429ebf270a1fdf88900677ff1a61c
SuperSpy827 <khushjammu@Khushs-MacBook-Pro.local>
2015-12-23 21:09:00 +0800

    Add the actual scripts.
    Add submap.py and relevant information.

---
+++ submap.py
@@ -0,0 +1,47 @@
+import socket
+from sys import argv
+'''
+TODO: - Use argpparse instead of argv
+      - Actually make dns requests
+'''
+try:
+    script, target = argv
+except ValueError:
+    print "Error: Please specify a target."
+    exit()
+# Define info
+info = '''
+Author: Colly
+Version: 1.0
+'''
+# Define the banner
+banner = '''
+   _____       _         _                       _         ______ _           _
+  / ____|     | |       | |                     (_)       |  ____(_)         | |
+ | (___  _   _| |__   __| | ___  _ __ ___   __ _ _ _ __   | |__   _ _ __   __| | ___ _ __
+  \___ \| | | | '_ \ / _` |/ _ \| '_ ` _ \ / _` | | '_ \  |  __| | | '_ \ / _` |/ _ \ '__|
+  ____) | |_| | |_) | (_| | (_) | | | | | | (_| | | | | | | |    | | | | | (_| |  __/ |
+ |_____/ \__,_|_.__/ \__,_|\___/|_| |_| |_|\__,_|_|_| |_| |_|    |_|_| |_|\__,_|\___|_|
+'''
+# Make a list directly from the text file using .split()
+listedsubdomains = open('subs.txt').read().split()
+# Define the scan() function
+def scan(hostname):
+    for hostname in listedsubdomains:
+        try:
+            print socket.gethostbyname(hostname+"."+target)
+            print hostname+"."+target+"\n"
+        except socket.gaierror: # Handle it being an invalid subdomain
+            hostname = False
+        except KeyboardInterrupt as error: # Handle the KeyboardInterrupt separately
+            print "\nScan interrupted by user."
+            error = error
+            exit()
+
+def intro():
+    print banner,info
+    #print info
+
+if __name__ == '__main__':
+    intro()
+    scan(target)
